4e989449fec4b8d31e0979f619ee035bf4c06d54
marce <9719marcel@gmail.com>
2018-05-05 12:26:51 +0200

    Added static files and updated homepage.

--- food/views.py
+++ food/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 # Create your views here.
+
+
+def food_view(request):
+    return render(request, 'pushEat/homepage.html')
+
+
+def profile_view(request):
+    return render(request, 'users/profile.html')
