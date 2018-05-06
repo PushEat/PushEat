2180c066e6099cb19901f42f2895cf666b09edd4
marce <9719marcel@gmail.com>
2018-05-06 20:15:52 +0200

    Solved logout bug.

--- food/users_urls.py
+++ food/users_urls.py
@@ -5,7 +5,6 @@ from views import *
 urlpatterns = [
     url(r'^login/$', staff_login, name="login"),
     url(r'^auth/$', auth_view, name="auth_view"),
-    url(r'^logout/$', logout, name="logout"),
     url(r'^loggedin/$', loggedin, name="loggedin"),
     url(r'^invalid/$', invalid_login, name="invalid_login"),
 ]
