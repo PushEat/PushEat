755864f08eb0b1a857c1e425bf99665d1b316300
marce <9719marcel@gmail.com>
2018-05-22 20:03:41 +0200

    Updated auctions system and food system also generated api system.

--- food/users_urls.py
+++ food/users_urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
+
 from food.auth_views import *
 from food.register_views import register
 from views import *
@@ -11,5 +13,7 @@ urlpatterns = [
     url(r'^register/', register, name="user_register"),
     url(r'^add_auctions/$', AddAuctions.as_view(), name='add_auctions'),
     url(r'^add_food/$', AddFoods.as_view(), name='add_food'),
-    url(r'^new_auction/$', auction_view, name='auction_view'),
+    url(r'^new_auction/(?P<pk>\d+)', auction_view, name='auction_view'),
+    url(r'^new_food/$', food_add_view, name='food_view'),
+    url(r'^api/$', TemplateView.as_view(template_name="users/api.html"), name='food_view'),
 ]
