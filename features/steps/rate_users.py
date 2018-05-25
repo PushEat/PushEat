1e137ebcfcb9d66a95542e6601d1c9fbd1f4589e
Korakk <oscarlopezluna@gmail.com>
2018-05-26 00:53:53 +0200

    Last feature repaired, but it was implemented just in a bad way

--- features/steps/rate_users.py
+++ features/steps/rate_users.py
@@ -1,12 +1,13 @@
 from behave import *
-
+import time
 use_step_matcher("re")
 
 
 @then("I rate up a user")
 def step_impl(context):
     context.browser.visit(context.get_url('/users/users_list'))
-    value=context.browser.find_by_id('value')
     context.browser.find_by_id('btnStar').first.click()
-    for i, row in enumerate(context.table):
-        assert row['rate'] != value[i].text
+    star = context.browser.find_by_id('value')
+
+    for i,row in enumerate(context.table):
+        assert row['rate'] in star[i].text
